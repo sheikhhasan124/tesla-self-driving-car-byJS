a6fe74ec9c6b7dcb41ac01e895b0a6efc8c79335
hasan-ullah <mdhasanullah124@gmail.com>
2022-05-13 16:00:15 +0600

    solving auto rotate with out speed and backward running issues

--- carDrivingMechnics/car.js
+++ carDrivingMechnics/car.js
@@ -7,8 +7,11 @@ class Car{
 
          this.speed = 0;
          this.acceleration=0.2;
+
          this.maxSpeed=3;
          this.friction=0.05;  
+
+         this.angle=0;
           
         this.controls= new Controls()
     }
@@ -45,24 +48,39 @@ class Car{
         }
 
         // left right moving fuction
-        if(this.controls.left){
-            this.x-=2;
+        // stop rotate when speed zero and when go backward and right it gose left that do not work in real life...solve it
+        if(this.speed!=0){
+            const flip=this.speed>0?1:-1;
+            if(this.controls.left){
+                // this.x-=2;
+                this.angle+=0.03*flip;
+            }
+            if(this.controls.right){
+                // this.x+=2;
+                this.angle-=0.03*flip;
+            } 
         }
-        if(this.controls.right){
-            this.x+=2;
-        } 
-
-        this.y-=this.speed;
+  //car rotate like tank but when running donot going left right,,,,solve this
+        this.x-=Math.sin(this.angle)*this.speed;
+        this.y-=Math.cos(this.angle)*this.speed;
     }
 
     draw(ctx){
+         ctx.save()
+         ctx.translate(this.x,this.y);
+         ctx.rotate(-this.angle);
+
         ctx.beginPath();
         ctx.rect(
-            this.x-this.width/2,
-            this.y-this.height/2,
+            // this.x-this.width/2,
+            // this.y-this.height/2,
+            -this.width/2,
+            -this.height/2,
             this.width,
             this.height
         )
         ctx.fill()
+
+        ctx.restore();
     }
 }
